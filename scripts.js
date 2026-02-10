8591567a263e82060efbe844f2dd999ad3490a23
Sincrata <catgal25@hotmail.com>
2026-02-10 03:42:42 -0500

    Base

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const head = document.getElementById("header");
+head.innerHTML = `
+    <img src="https://i.imgur.com/X76in4a.jpeg" alt="Website Banner" class="banner">
+
+    <nav>
+        <ul id="button-bar">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="https://toyhou.se/~world/53663.the-candyfloss">World</a></li>
+        </ul>
+    </nav>
+`;
+
+
+
